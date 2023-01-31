faac7911b7d2747d837118fd9c3ffc772688fcf4
Amysoj J A Jithisha <amysojjalouis@gmail.com>
2023-01-31 19:22:02 +0530

    updated readme.md

--- app.py
+++ app.py
@@ -1,44 +1,63 @@
-from flask import Flask, jsonify
-import json
-import threading
-import time
-import feedparser
-import json
+# Import the required libraries
 import re
 import datetime
+import feedparser
+from flask import Flask, jsonify
 
+# Compile a regular expression pattern to remove HTML tags
 CLEANR = re.compile('<.*?>')
-search_term = 'gold'  # change the search term based on your needs
+# Set search term to 'gold'
+search_term = 'gold'
+
+# Define a function to convert RSS feed to API data
 
 
 def rss_to_api():
     try:
+        # List of RSS Feed URLs
         url = ['https://www.business-standard.com/rss/markets-commodities-10608.rss', 'https://www.livemint.com/rss/markets',
                'https://www.thehindubusinessline.com/markets/commodities/feeder/default.rss']
+        # List of publishers for each RSS Feed
         publisher_name = ['Business Standard',
                           'Livemint', 'The HinduBusinessLine']
+        # Dictionary to store all post details
         posts_details = {}
+        # List to store individual post details
         post_list = []
+
+        # Iterate through all the RSS Feed URLs
         for i in range(len(url)):
+            # Parse the RSS Feed URL
             blog_feed = feedparser.parse(url[i])
+            # Get all posts in the RSS Feed
             posts = blog_feed.entries
 
+            # Iterate through each post
             for post in posts:
+                # Temporary dictionary to store post details
                 temp = dict()
                 try:
                     post_titles = []
+                    # Split the post title into words
                     post_titles.append(list(post.title.split(" ")))
+                    # Iterate through each word in the post title
                     for j in range(len(post_titles[0])):
+                        # Check if the word matches the search term
                         if (post_titles[0][j].lower() == search_term):
+                            # Clean post title to remove HTML tags
                             x = re.sub("\u20b9", "Rs ", re.sub(
-                                CLEANR, '', post.title))                             # change the currency symbol according to your needs
-
+                                CLEANR, '', post.title))
+                            # Store cleaned post title
                             temp["title"] = x
+                            # Store post link
                             temp["link"] = post.link
+                            # Clean post summary to remove HTML tags
                             y = re.sub(CLEANR, '', post.summary)
+                            # Store cleaned post summary
                             temp["summary"] = y
 
                             try:
+                                # Convert post published date to timestamp
                                 element = datetime.datetime.strptime(
                                     post.published, "%a, %d %b %Y %H:%M:%S %z")
                                 temp["published"] = int(
@@ -50,29 +69,38 @@ def rss_to_api():
                                     post.published, "%a, %d %b %Y %H:%M:%S %z")
                                 temp["published"] = int(
                                     datetime.datetime.timestamp(element))
-                            temp["author"] = publisher_name[i]
-
+                            # Store publisher name
                             temp["author"] = publisher_name[i]
                         else:
                             pass
                 except:
                     pass
 
+                # Append post details to post list
                 post_list.append(temp)
+        # Remove empty items from post list
         post_list = list(filter(None, post_list))
+        # Store post list in posts_details dictionary
         posts_details["posts"] = post_list
+        # Return the posts_details
         return posts_details
     except Exception as e:
+        # Print the error
         print(e)
 
 
+# Creating a Flask object
 app = Flask(__name__)
+
+# Setting the endpoint for HTTP GET requests
 
 
 @app.route('/', methods=['GET'])
 def welcome():
+    # Returns the data returned by rss_to_api() in a JSON format
     return jsonify({"data": rss_to_api()})
 
 
+# Running the Flask app with host IP 0.0.0.0 and port 8000
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="8000")
--- readme.md
+++ readme.md
@@ -1,22 +1,50 @@
-# Rss to Api #
-Hi, I have created an api that will allow you to convert rss to api. It filters all the rss feeds containing the word "gold". I have used Rss feed from 3 News firms
+# Rss to Api
 
-### Example Output ###
+This is a Flask based web application that converts RSS feed to API data. The application searches for posts in 3 different RSS Feeds, related to the search term 'gold'. The data extracted from the RSS Feeds is cleaned to remove HTML tags, stored in a list of dictionaries, and returned in JSON format when a GET request is made to the endpoint /.
 
+## Requirements
+
+- Flask
+- re
+- datetime
+- feedparser
+
+## Functionality
+
+The function `rss_to_api()` performs the following actions:
+
+- Iterates through a list of RSS Feed URLs, parsing each feed using `feedparser.parse(url[i])`.
+- For each feed, it iterates through the list of posts and checks if the post title contains the search term 'gold'.
+- If the post title contains the search term 'gold', the function cleans the post title and summary to remove HTML tags, converts the published date to a timestamp, and stores the post details (title, link, summary, published, author) in a temporary dictionary.
+- The temporary dictionary is then added to a list of dictionaries, which contains the post details for all posts in all the RSS Feeds.
+- The list of dictionaries is stored in another dictionary with the key posts, which is returned by the function `rss_to_api()`.
+
+## API Endpoint
+
+- The Flask application has only one endpoint / that accepts HTTP GET requests.
+- When a GET request is made to the endpoint, the function rss_to_api() is called and its returned data is sent in JSON format as a response to the GET request.
+
+## Running the Application
+
+- Clone the repository.
+- Navigate to the cloned repository in your terminal.
+- Install the required libraries
+- Run the Flask application by executing python filename.py
+- The application is now accessible at http://localhost:8000/ in your web browser.
+
+### Example Output
+
+```
 {
-    "data": {
-        "posts": [
-            {
-                "author": "Livemint",
-                "link": "https://www.livemint.com/market/commodities/gold-prices-today-rise-but-still-down-sharply-from-last-week-s-highs-11669301426017.html",
-                "published": 1669302891,
-                "summary": "In India, gold prices had risen to  ₹53,200 per 10 gram last week.",
-                "title": "Gold prices today rise but still down sharply from last week's highs"
-            }
-        ]
+"data": {
+    "posts": [
+        {
+            "author": "Business Standard",
+            "link": "https://www.business-standard.com/article/markets/global-gold-demand-grows-18-annually-in-2022-highest-since-2011-wgc-123013100844_1.html",
+            "published": 1675156080,
+            "summary": "Global gold demand grew 18 per cent annually to touch 4,741 tonnes in 2022, the highest since 2011, mainly driven by strong quarter four (October-December) demand and hefty central bank-buying, the World Gold Council (WGC) said in a report on Tuesday.\nThe total demand during 2021 was 4,012.8 tonnes, according to WGC's annual 'Gold Demand Trends' report.\nAnnual central bank demand more than doubled to 1,136 tonnes in 2022, up from 450 tonnes in the previous year and to a new 55-year high.\nPurchases in the fourth quarter last year alone reached 417 tonnes, bringing the total for the second half of 2022 to more than 800 tonnes, the report stated.\nInvestment demand (excluding over the counter) in 2022 was up 10 per cent against the previous year, mainly due to a notable slowdown in exchange-traded fund (ETF) outflows and strong gold bar and coin demand.\nGold bars and coins continued to hold favour with investors in several countries, which helped to offset weakness in China.\nTotal ..",
+            "title": "Global gold demand grows 18% annually in 2022, highest since 2011: WGC"
+        }]
     }
 }
-
-PS: If you have suggestions to the code please share at amysojjalouis@gmail.com.
-
-Hope you have a good day, happy coding :)
+```
